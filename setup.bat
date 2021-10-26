1b6e7fac64d83a4c7788e2dafc07f86208583e45
Seth Kooiker <kooikerseth@gmail.com>
2021-10-26 17:49:43 -0500

    added buffer stop and restarts

--- setup.bat
+++ setup.bat
@@ -5,11 +5,21 @@ docker run -dt^
     --name tianocore_container^
     --mount type=bind,source=%CD%,target=/src/mnt^
     tiano_image_build
+docker stop tianocore_container
+docker start tianocore_container
+
 docker exec -it tianocore_container git clone https://github.com/tianocore/edk2 ../
 docker exec -it tianocore_container git submodule update --init
+
+docker stop tianocore_container
+docker start tianocore_container
+
 docker exec -it tianocore_container make -C BaseTools
 docker exec -it tianocore_container bash edksetup.sh
-docker exec -it tianocore_container export EDK_TOOLS_PATH=/src/mnt/edk2/BaseTools
+
+docker stop tianocore_container
+docker start tianocore_container
+
 docker exec -it tianocore_container bash edksetup.sh BaseTools
 
 docker stop tianocore_container
